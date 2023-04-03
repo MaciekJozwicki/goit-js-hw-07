a0c84c1a0acc793899b2b93e89aa1cd61a9c25ab
Maciek <118013298+MaciekJozwicki@users.noreply.github.com>
2023-04-04 00:38:40 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery")
 
-console.log(galleryItems);
+
+
+galleryItems.forEach((item) => {
+    const galleryItemEl = `
+    <div class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            alt="${item.description}"
+            />
+        </a>
+    </div>`
+
+    galleryEl.insertAdjacentHTML("beforeend", galleryItemEl)
+
+})
+
+galleryEl.addEventListener("click", (event) => {
+    event.preventDefault()
+    console.log("tak")
+})
+
+
+
+
+
+
+
+
+
+
+
+
+console.log();
